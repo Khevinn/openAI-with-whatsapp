e447a25ff6cc74f7700ce8e3d6abcedf7091c0f3
Khevin Karlos <karimoxiha@gmail.com>
2023-02-16 14:30:59 -0300

    add vercel config to complete build and run server

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-qrcode.generate("http://github.com", (qrcode) => {
-  console.log(qrcode);
-});
-
 async function chatGPT(message) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
